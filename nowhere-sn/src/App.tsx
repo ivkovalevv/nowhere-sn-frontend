050618ab124d8a1d46ddafcb3138a79a0f5659bc
ivkovalevv <127996062+ivkovalevv@users.noreply.github.com>
2025-09-25 07:08:52 +0000

    add themes

--- nowhere-sn/src/App.tsx
+++ nowhere-sn/src/App.tsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [lightTheme, setLightTheme] = useState(() => {
+    const savedTheme = localStorage.getItem('darkTheme');
+    return savedTheme ? JSON.parse(savedTheme) : false;
+  });
+
+   useEffect(() => {
+    if (lightTheme) {
+      document.body.classList.add('light-theme');
+      document.body.classList.remove('dark-theme');
+    } else {
+      document.body.classList.add('dark-theme');
+      document.body.classList.remove('light-theme');
+    }
+    
+    localStorage.setItem('darkTheme', JSON.stringify(lightTheme));
+  }, [lightTheme]);
   return (
     <div className="App">
       Hello, Nowhere sn!
