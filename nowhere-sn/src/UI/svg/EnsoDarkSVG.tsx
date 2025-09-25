4590c9cac373817d734cf2b9c0c20d2fd84b816b
ivkovalevv <127996062+ivkovalevv@users.noreply.github.com>
2025-09-25 16:09:19 +0000

    shange header styles

--- nowhere-sn/src/UI/svg/EnsoDarkSVG.tsx
+++ nowhere-sn/src/UI/svg/EnsoDarkSVG.tsx
@@ -4,8 +4,8 @@ const EnsoDarkSVG = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     xmlnsXlink="http://www.w3.org/1999/xlink"
-    width="38"
-    height="38"
+    width="28"
+    height="28"
     fill="none"
     viewBox="0 0 38 38"
   >
